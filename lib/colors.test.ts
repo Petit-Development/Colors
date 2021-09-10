5c9298ff05a8d99aa8a0c7225de2bc068fea2ae0
Joshua Petitma <joshua@petit.dev>
2021-09-10 09:01:26 -0400

    Creat tests for generate color function

--- lib/colors.test.ts
+++ lib/colors.test.ts
@@ -1,3 +1,49 @@
-test('Stuff works', () => {
-  expect(3).toBe(3);
+import { generateColor } from "@lib/colors";
+import { PassThrough } from "stream";
+import * as fs from "fs";
+
+// Generate color files to manually validate them
+const generateFile = false;
+
+// These tests just validate that nothing errors out
+test("Can generate pngs", () => {
+  const mockedStream = new PassThrough();
+  if (generateFile) {
+    const out = fs.createWriteStream("image.png");
+    mockedStream.pipe(out);
+  }
+  generateColor(mockedStream, {
+    color: "#0000A0",
+    filetype: "png",
+    height: 3000,
+    width: 200,
+  });
+});
+
+test("Can generate pdfs", () => {
+  const mockedStream = new PassThrough();
+  if (generateFile) {
+    const out = fs.createWriteStream("image.pdf");
+    mockedStream.pipe(out);
+  }
+  generateColor(mockedStream, {
+    color: "#0000A0",
+    filetype: "pdf",
+    height: 3000,
+    width: 200,
+  });
+});
+
+test("Can generate jpegs", () => {
+  const mockedStream = new PassThrough();
+  if (generateFile) {
+    const out = fs.createWriteStream("image.jpg");
+    mockedStream.pipe(out);
+  }
+  generateColor(mockedStream, {
+    color: "#0000A0",
+    filetype: "jpeg",
+    height: 3000,
+    width: 200,
+  });
 });
