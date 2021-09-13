8e54b90657052285760f3ecc856a8147c2d70c28
Joshua Petitma <joshua@petit.dev>
2021-09-13 14:13:43 -0400

    Create temporary boxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -11,12 +11,19 @@ const Home: NextPage = () => {
         <meta name="description" content="Easily view colors in the browser" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <span className="title">Title</span>
+        <h1>Heading 1</h1>
+        <h2>Heading 2</h2>
+        <h3>Heading 3</h3>
+        <p>body</p>
+        <span className="subtitle">subtitle</span>
+        <div className={styles.temp1}>color1</div>
+        <div className={styles.temp2}>color</div>
 
-      <p className="title">Title</p>
-      <h1>Heading 1</h1>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
