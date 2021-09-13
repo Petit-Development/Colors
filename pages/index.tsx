42f21d2ba07a175a48473be753e12fb7bd5607f6
Joshua Petitma <joshua@petit.dev>
2021-09-13 15:22:58 -0400

    Create example color updating

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
 
 const Home: NextPage = () => {
   return (
@@ -18,9 +18,20 @@ const Home: NextPage = () => {
         <h3>Heading 3</h3>
         <p>body</p>
         <span className="subtitle">subtitle</span>
-        <div className={styles.temp1}>color1</div>
-        <div className={styles.temp2}>color</div>
+        <div>
+          <span className={styles.temp1}>color1</span>
+        </div>
+        <div className={styles.temp2}>
+          <span className={styles.temp2wrapper}>color2</span>
+        </div>
 
+        <button
+          onClick={() => {
+            document.body.style.setProperty("--main-color", "green");
+          }}
+        >
+          Click me To Go Crazy!
+        </button>
       </div>
     </div>
   );
