50589c17c0c1a91f4ca0d040141864d7a01af9ba
Joshua Petitma <joshua@petit.dev>
2021-09-17 14:33:53 -0400

    Use secondary color for non-background elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import createPalette from "@lib/palette";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
@@ -11,15 +12,18 @@ const Home: NextPage = () => {
         <meta name="description" content="Easily view colors in the browser" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="title">Colors.</div>
+      <div className="title">Colors</div>
       <div className={styles.bubbles}>#7F87BD</div>
       <input type="text" />
+      <button onClick={() => updateColor('green')}>click</button>
     </div>
   );
 };
 
 const updateColor = (color: string) => {
-  document.body.style.setProperty("--main-color", color);
+  const palette = createPalette('##7F87BD');
+  document.body.style.setProperty("--main-color", palette.primary);
+  document.body.style.setProperty("--main-color", palette.secondary);
 };
 
 export default Home;
