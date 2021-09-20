23864a6742a1690b65f901d4109bf2bacb05885f
Joshua Petitma <joshua@petit.dev>
2021-09-19 20:55:01 -0400

    Add stylings for controls

--- components/controls/controls.tsx
+++ components/controls/controls.tsx
@@ -1,13 +1,22 @@
 import IconButton from "@components/buttons/iconButton";
-import styles from '@styles/Controls.module.scss';
+import TextButton from "@components/buttons/textButton";
+import styles from "@styles/Controls.module.scss";
 
 interface ControlsProps {
   onSettingsClicked: () => void;
 }
-
+console.log(styles);
 const Controls: React.FC<ControlsProps> = ({}) => (
   <div className={styles.container}>
     <IconButton className={styles.play} icon="play" onClick={() => {}} />
+    <div className={styles.options}>
+      <TextButton className={styles.export} text="Export" onClick={() => {}} />
+      <IconButton
+        className={styles.settings}
+        icon="settings"
+        onClick={() => {}}
+      />
+    </div>
   </div>
 );
 
