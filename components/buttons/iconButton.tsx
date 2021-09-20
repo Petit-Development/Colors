70483e013d385aae9d2ebcd972a6e426af9e9295
Joshua Petitma <joshua@petit.dev>
2021-09-19 20:38:35 -0400

    Show classname directly

--- components/buttons/iconButton.tsx
+++ components/buttons/iconButton.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 const IconButton: FC<ButtonProps> = ({ onClick, icon, className }) => {
   const sub = icon == "play" ? "play_arrow" : "settings";
   return (
-    <div onClick={onClick} className={`${styles.button} ${className && styles[className]}`}>
+    <div onClick={onClick} className={`${styles.button} ${className}`}>
       <i className={"material-icons"}>{sub}</i>
     </div>
   );
